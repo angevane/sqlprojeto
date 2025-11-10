b3d1d51bb924e4f7f145e75ad37fa0a86574afa0
Angevane <ladutra144@gmail.com>
2025-11-10 13:29:40 -0300

    adicionando 5 livros

--- main.py
+++ main.py
@@ -18,3 +18,23 @@ cursor.execute("""
 print("Tabela criada com sucesso!")
 
 
+import sqlite3
+con = sqlite3.connect("Livro.db")
+cur = con.cursor()
+
+livros = [
+    ('A Sombra do Vento', 'Carlos Ruiz Zafón', 2001, 'Mistério', 1),
+    ('1984', 'George Orwell',1949, 'Ficção Científica', 0),
+    ('O Senhor dos Anéis', 'J.R.R. Tolkien',1954, 'Fantasia', 1),
+    ('Dom Quixote', 'Miguel de Cervantes',1605, 'Romance', 1),
+    ('Cem Anos de Solidão', 'Gabriel García Márquez',1967, 'Realismo Mágico', 0)
+]
+
+cur.executemany("""
+                INSERT INTO Livros (titulo, autor, ano, genero, disponivel)
+                VALUES(?, ?, ?, ?, ?)
+                """, livros)
+
+con.commit()
+print("Livros inseridos com sucesso!")
+con.close()
