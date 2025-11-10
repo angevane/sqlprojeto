31e3b2d108fef75d239b20a37ade5c115f5106a4
Angevane <ladutra144@gmail.com>
2025-11-10 13:10:01 -0300

    Criando a Tabela Livros

---
+++ main.py
@@ -0,0 +1,18 @@
+import sqlite3
+
+
+executar = sqlite3.connect("Livro.db")
+
+cursor = executar.cursor()
+
+cursor.execute("""
+               CREATE TABLE IF NOT EXISTS Livros (
+               id INTEGER PRIMARY KEY AUTOINCREMENT, 
+               titulo TEXT NOT NULL UNIQUE, 
+               autor TEXT, 
+               ano INTEGER,
+               genero TEXT, 
+               disponivel INTEGER)
+                """)
+
+print("Tabela criada com sucesso!")
